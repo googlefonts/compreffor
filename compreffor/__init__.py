37d6eeab6dce0603bb9e12cb17da5dec72438d03
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-05-11 09:15:51 +0200

    compreffor.__init__: set only two Methods (Cxx or Py); default is using C++ extension module

--- compreffor/__init__.py
+++ compreffor/__init__.py
@@ -23,7 +23,7 @@ interface, this tool can be used on the command line.
 Usage (python):
 >>> font = TTFont(filename)
 >>> options = { ... }
->>> comp = Compreffor(font, method=compreffor.Methods.CxxLib, **options)
+>>> comp = Compreffor(font, method=compreffor.Methods.Cxx, **options)
 >>> comp.compress()
 >>> font.save(filename)
 
@@ -42,26 +42,20 @@ With Methods.Py, the following additional options are available:
                              to run
 
 Compression Backends:
-There are 3 different ways the compreffor can be run.
+There are 2 different ways the compreffor can be run.
     - First is a pure python approach, which can be selected from this module
       by passing method=Methods.Py. This backend is significantly slower than
       the other 2 backends (~10-20x). The logic for this backend can be found
       in pyCompressor.py.
-    - Second is a C++ executable backed approach. With this method, data is
-      prepared in python and then the executable `cffCompressor` is started
-      and data piped in. It's response is read back in over another pipe and
-      python writes the result. This can be selected here using
-      Methods.CxxExecutable and logic can be found in cxxCompressor.py,
-      cffCompressor.h, and cffCompressor.cc.
-    - Third is C++ dynamic library (shared object) backed. With this method,
+    - Second is C++ extension module backed. With this method,
       python calls the relevant functions directly from `libcompreff.so` rather
       than starting an executable in another process. This is currently slower
       than the second approach. This can be selected here using Methods.CxxLib
       and again logic is in cxxCompressor.py, cffCompressor.h, and
       cffCompressor.cc.
 If a Compreffor is initialized with no method, Methods.NoPref is chosen. This
-method automatically chooses the fastest available backend (order is exe, lib,
-py). Availability is dependent on the presence of the compiled binary and library.
+method automatically chooses the fastest available backend (order is c++,
+py). Availability is dependent on the presence of the compiled module.
 
 Usage (command line):
 To use on the command line, pyCompressor.py or cxxCompressor.py must be called
@@ -81,48 +75,36 @@ extension. Example usage:
 """
 
 from compreffor import cxxCompressor, pyCompressor
-import os
-import sys
 
-
-# platform-specific executable name
-EXE_NAME = 'cffCompressor'
-if sys.platform == 'win32':
-    EXE_NAME += '.exe'
-
-# platform-specific shared library name
-if sys.platform == 'win32':
-    LIB_NAME = 'compreff.dll'
+try:
+    from compreffor import _compreffor
+except ImportError:
+    use_cxx = False
 else:
-    LIB_NAME = 'libcompreff.so'
+    use_cxx = True
 
 
 class Methods:
-    NoPref, Py, CxxExecutable, CxxLib = range(4)
+    NoPref, Py, Cxx = range(3)
+
 
 class Compreffor(object):
     def __init__(self, font, method=Methods.NoPref, **options):
         self.font = font
         self.method = method
         self.options = options
-        self.exe_path = os.path.join(os.path.dirname(__file__), EXE_NAME)
-        self.lib_path = os.path.join(os.path.dirname(__file__), LIB_NAME)
 
     def compress(self):
         if self.method == Methods.NoPref:
             # choose fastest available method
-            if os.path.exists(self.exe_path):
-                self.run_executable()
-            elif os.path.exists(self.lib_path):
-                self.run_lib()
+            if use_cxx:
+                self.run_cxx()
             else:
                 self.run_py()
         elif self.method == Methods.Py:
             self.run_py()
-        elif self.method == Methods.CxxExecutable:
-            self.run_executable()
-        elif self.method == Methods.CxxLib:
-            self.run_lib()
+        elif self.method == Methods.Cxx:
+            self.run_cxx()
         else:
             assert 0
 
@@ -130,10 +112,5 @@ class Compreffor(object):
         compreffor = pyCompressor.Compreffor(self.font, **self.options)
         compreffor.compress()
 
-    def run_executable(self):
-        assert os.path.exists(self.exe_path)
-        cxxCompressor.compreff(self.font, use_lib=False, **self.options)
-
-    def run_lib(self):
-        assert os.path.exists(self.lib_path)
-        cxxCompressor.compreff(self.font, use_lib=True, **self.options)
+    def run_cxx(self):
+        cxxCompressor.compreff(self.font, **self.options)
