fa3fd126034d001766759a8c22e49975437a1614
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-08-23 01:23:00 +0200

    [config.sh] use tox to run test against pre-compiled wheel
    call tox with test environment based on $PYTHON_VERSION

---
+++ config.sh
@@ -0,0 +1,34 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # The function is called from an empty temporary directory.
+    # Get absolute path to the pre-compiled wheel
+    wheelhouse=$(abspath ../wheelhouse)
+    wheel=`ls ${wheelhouse}/compreffor*.whl | head -n 1`
+
+    # select tox environment
+    if [ -n "$IS_OSX" ]; then
+        PYTHON_VERSION=$MB_PYTHON_VERSION
+    fi
+    case "${PYTHON_VERSION}" in
+        2.7)
+           TOXENV=py27
+           ;;
+        3.4)
+           TOXENV=py34
+           ;;
+        3.5)
+           TOXENV=py35
+           ;;
+    esac
+
+    # Runs tests on installed wheel
+    tox --installpkg $wheel -e $TOXENV
+}
