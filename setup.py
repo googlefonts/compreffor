6d54a571ecb400475c32459767d7606d21b23448
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-09-28 11:20:29 +0100

    [setup.py] add PyPI Trove Classifiers

--- setup.py
+++ setup.py
@@ -173,6 +173,18 @@ setup_params = dict(
         "fonttools>=3.1",
     ],
     zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+    ],
 )
 
 
