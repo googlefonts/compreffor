892b2e8b3300123e3c6e8e35b7fc56a9461820c8
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-10-09 18:13:58 +0100

    cxxCompressor: remove main(); merged into compreffor.__main__
    for pep8, ensure two empty lines between module-level functions

--- src/python/compreffor/cxxCompressor.py
+++ src/python/compreffor/cxxCompressor.py
@@ -31,18 +31,14 @@ Usage (python):
 >> font.save("/path/to/output.otf")
 """
 
-import argparse
 import array
 import struct
 import time
 import os
 from compreffor.pyCompressor import (
-    Compreffor, CandidateSubr, tokenCost, human_size)
-from compreffor.test.util import (
-    check_compression_integrity, check_call_depth)
+    Compreffor, CandidateSubr, tokenCost)
 from compreffor import _compreffor as lib
 from fontTools.misc.py23 import BytesIO
-from fontTools.ttLib import TTFont
 
 
 __all__ = ["compreff"]
@@ -51,6 +47,7 @@ __all__ = ["compreff"]
 NSUBRS_LIMIT = 65533
 SUBR_NEST_LIMIT  = 10
 
+
 class IdKeyMap(object):
     """A map that where every key's value is itself. Used
     as a map from simplified key space to actual key space
@@ -58,6 +55,7 @@ class IdKeyMap(object):
 
     def __getitem__(self, tok):
         return tok
+
 
 class SimpleCandidateSubr(CandidateSubr):
     """A reimplimentation of CandidateSubr to be more
@@ -85,6 +83,7 @@ class SimpleCandidateSubr(CandidateSubr):
     def encoding(self):
         return self._encoding
 
+
 def write_data(td):
     """Writes CharStrings and FDSelect from the TopDict td into a string
     that is easily readable."""
@@ -97,6 +96,7 @@ def write_data(td):
         fdselect = struct.pack('B', 1)
     out.write(fdselect)
     return out.getvalue()
+
 
 def get_encoding(data_buffer, subrs):
     """Read a charstring's encoding stream out of a string buffer response
@@ -114,6 +114,7 @@ def get_encoding(data_buffer, subrs):
         subrs[subr_index].freq += 1
         enc.append((insertion_pos, subrs[subr_index]))
     return enc, pos
+
 
 def read_data(td, result_string):
     """Read the output of cffCompressor.cc into Python data
@@ -147,6 +148,7 @@ def read_data(td, result_string):
 
     assert pos == len(results)
     return (subrs, glyph_encodings)
+
 
 def interpret_data(td, results):
     """Interpret the result array from a lib.compreff call to
@@ -190,6 +192,7 @@ def interpret_data(td, results):
         glyph_encodings.append(enc)
 
     return (subrs, glyph_encodings)
+
 
 def compreff(font, verbose=False, **kwargs):
     """Main function that compresses `font`, a TTFont object,
@@ -268,104 +271,3 @@ def compreff(font, verbose=False, **kwargs):
     if verbose:
         print("Finished post-processing (delta %gs)" % (time.time() - start_time))
         print("Total time: %gs" % (time.time() - full_start_time))
-
-def main(filename=None, comp_fname=None, test=False, decompress=False,
-         verbose=False, check=False, generate_cff=False, recursive=False,
-         **comp_kwargs):
-    if test:
-        pass
-
-    if filename and comp_fname == None:
-        def handle_font(font_name):
-            font = TTFont(font_name)
-
-            td = font['CFF '].cff.topDictIndex[0]
-            no_subrs = lambda fd: hasattr(fd, 'Subrs') and len(fd.Subrs) > 0
-            priv_subrs = (hasattr(td, 'FDArray') and
-                          any(no_subrs(fd) for fd in td.FDArray))
-            if len(td.GlobalSubrs) > 0 or priv_subrs:
-                print("Warning: There are subrs in %s" % font_name)
-
-            orig_size = os.path.getsize(font_name)
-
-            if decompress:
-                from fontTools import subset
-                options = subset.Options()
-                options.desubroutinize = True
-                subsetter = subset.Subsetter(options=options)
-                subsetter.populate(glyphs=font.getGlyphOrder())
-                subsetter.subset(font)
-
-            out_name = "%s.compressed%s" % os.path.splitext(font_name)
-
-            compreff(font, verbose=verbose, **comp_kwargs)
-
-            # save compressed font
-            start_time = time.time()
-            font.save(out_name)
-            if verbose:
-                print("Compiled and saved (took %gs)" % (time.time() - start_time))
-
-            if generate_cff:
-                # save CFF version
-                with open(os.path.splitext(out_name)[0] + ".cff", 'wb') as f:
-                    font['CFF '].cff.compile(f, None)
-
-            comp_size = os.path.getsize(out_name)
-            print("Compressed to %s -- saved %s" %
-                    (os.path.basename(out_name), human_size(orig_size - comp_size)))
-
-            if check:
-                check_compression_integrity(filename, out_name)
-                check_call_depth(out_name)
-
-        if recursive:
-            for root, dirs, files in os.walk(filename):
-                for fname in files:
-                    if os.path.splitext(fname)[1] == '.otf':
-                        handle_font(fname)
-        else:
-            handle_font(filename)
-
-    if check and comp_fname != None:
-        check_compression_integrity(filename, comp_fname)
-        check_call_depth(comp_fname)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                        description="""FontTools Compreffor will take a CFF-flavored
-                                       OpenType font and automatically detect
-                                       repeated routines and generate subroutines
-                                       to minimize the disk space needed to
-                                       represent a font.""")
-    parser.add_argument('filename', help="the path to the font file", nargs='?')
-    parser.add_argument('comp_fname', nargs='?', metavar='compressed-file',
-                        help="the path to the compressed file. if this is given"
-                             " with the -c flag, it will be checked against "
-                             " `filename`.")
-    # no tests yet :(
-    # parser.add_argument('-t', '--test', required=False, action='store_true',
-    #                     default=False, help="run test cases")
-    parser.add_argument('-v', '--verbose', required=False, action='store_true',
-                        dest='verbose', default=False)
-    parser.add_argument('-c', '--check', required=False, action='store_true',
-                        help="verify that the outputted font is valid and "
-                             "functionally equivalent to the input")
-    parser.add_argument('-d', '--decompress', required=False, action='store_true',
-                        help="decompress source before compressing (necessary if "
-                             "there are subroutines in the source)")
-    parser.add_argument('-r', '--recursive', required=False, action='store_true',
-                        default=False)
-    parser.add_argument('-n', '--nrounds', required=False, type=int,
-                        help="the number of iterations to run the algorithm"
-                             " (defaults to 4)")
-    parser.add_argument('-m', '--maxsubrs', required=False, type=int,
-                        dest='nsubrs_limit', help="limit to the number of "
-                                                  " subroutines per INDEX"
-                                                  " (defaults to 64K)")
-    parser.add_argument('--generatecff', required=False, action='store_true',
-                        dest='generate_cff', default=False)
-
-    kwargs = vars(parser.parse_args())
-
-    main(**kwargs)
