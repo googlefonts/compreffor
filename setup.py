a09e1ed4367f89d1029301d081f1e9ef2d74bf0e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-06-21 09:36:27 +0100

    Bump version: 0.4.4 → 0.4.5.dev0

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ with open('README.rst', 'r') as f:
 
 setup_params = dict(
     name="compreffor",
-    version="0.4.4",
+    version="0.4.5.dev0",
     description="A CFF subroutinizer for fontTools.",
     long_description=long_description,
     author="Sam Fishman",
