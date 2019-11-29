d840883f8b1fcf3721740b072ffcacc2c1a11b2b
Cosimo Lupo <clupo@google.com>
2019-11-29 12:55:50 +0000

    Bump version: 0.4.4 → 0.5.0b1

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ with open('README.rst', 'r') as f:
 
 setup_params = dict(
     name="compreffor",
-    version="0.4.7.dev0",
+    version="0.5.0b1",
     description="A CFF subroutinizer for fontTools.",
     long_description=long_description,
     author="Sam Fishman",
--- src/python/compreffor/__init__.py
+++ src/python/compreffor/__init__.py
@@ -68,7 +68,7 @@ timer = Timer(logger=logging.getLogger(log.name + ".timer"))
 from compreffor import cxxCompressor, pyCompressor
 
 
-__version__ = "0.4.7.dev0"
+__version__ = "0.5.0b1"
 
 
 def compress(ttFont, method_python=False, **options):
